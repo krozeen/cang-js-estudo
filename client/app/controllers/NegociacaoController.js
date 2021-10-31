76083eb9ad887b7186d134e7298ea4a9737e50be
João Vitor Pacheco de Freitas <joaovotk@gmail.com>
2021-10-30 22:10:20 -0300

    Tratamento da data enviada pelo usuario qual sera enviada para construtor da nova negociação

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -15,9 +15,27 @@ class NegociacaoController {
     //comando para nao recarregar pagina ao submitar o formulario
     event.preventDefault();
 
-    let negociacao = new Negociacao(this._inputData,
-                                    parseFloat(this._inputValor),
-                                    parseInt(this._inputQuantidade));
+    //Descontinuado
+    //let dataformatada = new Date(this._inputData.value.replace(/-/g, ','));
+
+    /*E utilizado spread operator para passar como parametro separadamente
+    o resultado do split do array */
+
+    //Spread trata cada elemento do array de forma individual
+    /*Função map é utilizada para tratar o mes (0 a 11), sendo 0 janeiro 
+     e 11 dezembro*/
+    let dataformatada = new Date(...this._inputData.value
+                                  .split('-')
+                                  .map((element, index) => {
+                                        if(index === 1){
+                                          return element - 1;
+                                        }
+                                       return element;
+                                  }));
+
+    let negociacao = new Negociacao(dataformatada,
+                                    parseFloat(this._inputValor.value),
+                                    parseInt(this._inputQuantidade.value));
 
 
     console.log(negociacao);
